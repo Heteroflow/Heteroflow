7fce8e5d76de4b391a7289864b6a33e7d92daf85
Tsung-Wei Huang <twh760812@gmail.com>
2019-08-15 22:59:21 -0600

    updated task

--- heteroflow/task/task.hpp
+++ heteroflow/task/task.hpp
@@ -3,28 +3,79 @@
 
 namespace hf {
 
-// Task
-class Task {
-
-  struct Host {
-  };
-
-  struct Pull {
-    void* d_data {nullptr};
-    void* h_data {nullptr};
-    size_t d_size {0};
-    size_t h_size {0};
-  };
-
-  struct Push {
-  };
+// TaskBase
+class TaskBase {
 
   public:
 
-    void precede(Task&);
+  virtual ~TaskBase() = default;
+
+  void precede(Task&);
 
   private:
 
+};
+
+// ----------------------------------------------------------------------------
+
+class HostTask : public HostTask {
+  
+  public:
+    
+    template <typename C>
+    HostTask(C&& callable);
+
+  private:
+
+};
+
+// ----------------------------------------------------------------------------
+
+// PullTask
+class PullTask : public TaskBase {
+
+  public:
+
+  PullTask(void* h_data, size_t h_size);
+
+  ~PullTask();
+
+  void pull(void* h_data, size_t h_size);
+
+  private:
+    
+  void* _h_data  {nullptr};
+  void* _d_data  {nullptr};
+  size_t _h_size {0};
+  size_t _d_size {0};
+};
+  
+inline PullTask(void* h_data, size_t h_size) : 
+  _h_data {h_data}, _h_size {h_size} {
+}
+
+inline ~PullTask() {
+  // TODO
+}
+
+inline void pull(void* h_data, size_t h_size) {
+  _h_data = h_data;
+  _h_size = h_size;
+}
+
+// ----------------------------------------------------------------------------
+
+class PushTask {
+
+  public:
+
+  private:
+
+    PullTask& _source;
+
+    void* _target {nullptr};
+
+    size_t _size {0};
 };
 
 // ----------------------------------------------------------------------------
